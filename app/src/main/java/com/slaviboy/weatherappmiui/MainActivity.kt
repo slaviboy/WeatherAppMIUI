dd309c8bb89e3e8433cba80fd4903958a6d00e11
slaviboy <slavi94slavi94@gmail.com>
2022-06-25 12:56:38 +0300

    Initial commit

---
+++ app/src/main/java/com/slaviboy/weatherappmiui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.slaviboy.weatherappmiui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
