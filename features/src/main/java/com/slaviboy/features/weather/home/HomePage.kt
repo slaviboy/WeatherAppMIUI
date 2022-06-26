e60990d4976fc5375168eb388a9f0356ea86040b
slaviboy <slavi94slavi94@gmail.com>
2022-06-26 18:51:46 +0300

    Add Forecast24Hours composable
    Signed-off-by: slaviboy <slavi94slavi94@gmail.com>

--- features/src/main/java/com/slaviboy/features/weather/home/HomePage.kt
+++ features/src/main/java/com/slaviboy/features/weather/home/HomePage.kt
@@ -1,11 +1,8 @@
 package com.slaviboy.features.weather.home
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -15,7 +12,6 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.font.FontWeight
 import com.slaviboy.composeunits.dw
 import com.slaviboy.composeunits.sw
@@ -23,7 +19,6 @@ import com.slaviboy.features.R
 import com.slaviboy.fonts.AnoFont
 import com.slaviboy.fonts.RobotoFont
 
-@OptIn(ExperimentalTextApi::class)
 @Composable
 fun HomePage() {
     Background()
@@ -36,113 +31,28 @@ fun HomePage() {
             modifier = Modifier
                 .verticalScroll(rememberScrollState())
         ) {
-
-            Row(
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(0.13.dw)
-                    .background(Color.Green)
-                    .padding(horizontal = 0.05.dw)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_plus),
-                    contentDescription = null,
-                    contentScale = ContentScale.FillHeight,
-                    modifier = Modifier
-                        .wrapContentWidth()
-                        .fillMaxHeight()
-                        .padding(0.038.dw)
-                )
-                Text(
-                    "Blagoevgrad",
-                    fontSize = 0.045.sw,
-                    fontFamily = RobotoFont,
-                    color = Color.White
-                )
-                Image(
-                    painter = painterResource(id = R.drawable.ic_more),
-                    contentDescription = null,
-                    contentScale = ContentScale.FillHeight,
-                    modifier = Modifier
-                        .wrapContentWidth()
-                        .fillMaxHeight()
-                        .padding(0.038.dw)
-                )
-            }
-
+            TopBar()
             Spacer(modifier = Modifier.height(0.3.dw))
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.Top,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .wrapContentHeight()
-                    .background(Color.Green)
-            ) {
-                Text(
-                    "26",
-                    fontSize = 0.34.sw,
-                    fontFamily = AnoFont,
-                    color = Color(0xFFDBE8F8),
-                    modifier = Modifier
-                        .background(Color.Blue)
-                )
-                Text(
-                    "°C",
-                    fontSize = 0.065.sw,
-                    fontWeight = FontWeight.Black,
-                    fontFamily = AnoFont,
-                    color = Color(0xFFDBE8F8),
-                    modifier = Modifier
-                        .offset(y = 0.05.dw)
-                )
-            }
-
-            Spacer(modifier = Modifier.height(0.02.dw))
-            Text(
-                "Clear",
-                fontSize = 0.06.sw,
-                fontFamily = AnoFont,
-                color = Color.White,
-                modifier = Modifier
-                    .background(Color.Blue)
-            )
-
+            MainBar()
             Spacer(modifier = Modifier.height(0.04.dw))
-            Row(
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .wrapContentWidth()
-                    .height(0.08.dw)
-                    .background(
-                        color = Color(0x33FFFFFF),
-                        shape = RoundedCornerShape(0.05.dw)
-                    )
-            ) {
-                Spacer(modifier = Modifier.width(0.01.dw))
-                Image(
-                    painter = painterResource(id = R.drawable.ic_leaf),
-                    contentDescription = null,
-                    contentScale = ContentScale.FillHeight,
-                    modifier = Modifier
-                        .wrapContentWidth()
-                        .fillMaxHeight()
-                        .padding(0.01.dw)
+            AirQualityIndex()
+            Spacer(modifier = Modifier.height(0.15.dw))
+            Forecast24Hours(
+                listOf(
+                    Forecast24HoursItem(),
+                    Forecast24HoursItem(),
+                    Forecast24HoursItem(),
+                    Forecast24HoursItem(),
+                    Forecast24HoursItem(),
+                    Forecast24HoursItem(),
+                    Forecast24HoursItem(),
+                    Forecast24HoursItem(),
+                    Forecast24HoursItem(),
+                    Forecast24HoursItem(),
+                    Forecast24HoursItem(),
+                    Forecast24HoursItem()
                 )
-                Spacer(modifier = Modifier.width(0.015.dw))
-                Text(
-                    "AQI 32",
-                    fontSize = 0.04.sw,
-                    fontFamily = AnoFont,
-                    color = Color.White,
-                    modifier = Modifier
-                        .background(Color.Blue)
-                )
-                Spacer(modifier = Modifier.width(0.02.dw))
-            }
+            )
         }
     }
 }
@@ -164,3 +74,209 @@ fun Background() {
             )
     )
 }
+
+@Composable
+fun TopBar() {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(0.13.dw)
+            .background(Color.Green)
+            .padding(horizontal = 0.05.dw)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_plus),
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier
+                .wrapContentWidth()
+                .fillMaxHeight()
+                .padding(0.038.dw)
+        )
+        Text(
+            "Blagoevgrad",
+            fontSize = 0.045.sw,
+            fontFamily = RobotoFont,
+            color = Color.White
+        )
+        Image(
+            painter = painterResource(id = R.drawable.ic_more),
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier
+                .wrapContentWidth()
+                .fillMaxHeight()
+                .padding(0.038.dw)
+        )
+    }
+}
+
+@Composable
+fun MainBar() {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.Top,
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .background(Color.Green)
+    ) {
+        Text(
+            "26",
+            fontSize = 0.32.sw,
+            fontFamily = AnoFont,
+            color = Color(0xFFDBE8F8),
+            modifier = Modifier
+                .background(Color.Blue)
+        )
+        Text(
+            "°C",
+            fontSize = 0.061.sw,
+            fontWeight = FontWeight.Black,
+            fontFamily = AnoFont,
+            color = Color(0xFFDBE8F8),
+            modifier = Modifier
+                .offset(y = 0.05.dw)
+        )
+    }
+
+    Spacer(modifier = Modifier.height(0.02.dw))
+    Text(
+        "Clear",
+        fontSize = 0.05.sw,
+        fontFamily = AnoFont,
+        color = Color.White,
+        modifier = Modifier
+            .background(Color.Blue)
+    )
+}
+
+@Composable
+fun AirQualityIndex() {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .wrapContentWidth()
+            .height(0.07.dw)
+            .background(
+                color = Color(0x33FFFFFF),
+                shape = RoundedCornerShape(0.05.dw)
+            )
+    ) {
+        Spacer(modifier = Modifier.width(0.01.dw))
+        Image(
+            painter = painterResource(id = R.drawable.ic_leaf),
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier
+                .wrapContentWidth()
+                .fillMaxHeight()
+                .padding(0.015.dw)
+        )
+        Spacer(modifier = Modifier.width(0.015.dw))
+        Text(
+            "AQI 32",
+            fontSize = 0.03.sw,
+            fontFamily = RobotoFont,
+            color = Color.White,
+            modifier = Modifier
+                .background(Color.Blue)
+        )
+        Spacer(modifier = Modifier.width(0.02.dw))
+    }
+}
+
+@Composable
+fun Forecast24Hours(forecast24HoursItems: List<Forecast24HoursItem>) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .horizontalScroll(rememberScrollState())
+            .wrapContentSize()
+            .background(Color.Black)
+    ) {
+        forecast24HoursItems.forEach { item ->
+            Spacer(modifier = Modifier.width(0.05.dw))
+            Forecast24HoursItem(item)
+            Spacer(modifier = Modifier.width(0.05.dw))
+        }
+    }
+}
+
+@Composable
+fun Forecast24HoursItem(forecast24HoursItem: Forecast24HoursItem) {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "16:00",
+            fontSize = 0.025.sw,
+            fontFamily = AnoFont,
+            color = Color(0xFFFEFFFF),
+            modifier = Modifier
+                .background(Color.Blue)
+        )
+        Spacer(modifier = Modifier.height(0.02.dw))
+        Text(
+            "33°",
+            fontSize = 0.045.sw,
+            fontFamily = AnoFont,
+            color = Color(0xFFFEFFFF),
+            modifier = Modifier
+                .background(Color.Blue)
+        )
+        Spacer(modifier = Modifier.height(0.02.dw))
+        Image(
+            painter = painterResource(id = R.drawable.ic_day_clear_sky),
+            contentDescription = null,
+            contentScale = ContentScale.FillHeight,
+            modifier = Modifier
+                .wrapContentWidth()
+                .height(0.07.dw)
+        )
+        Spacer(modifier = Modifier.height(0.02.dw))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .wrapContentSize()
+                .background(Color.Black)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_direction),
+                contentDescription = null,
+                contentScale = ContentScale.FillHeight,
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .height(0.033.dw)
+            )
+            Spacer(modifier = Modifier.width(0.01.dw))
+            Text(
+                "3.5km/h",
+                fontSize = 0.02.sw,
+                fontFamily = AnoFont,
+                color = Color(0xFFFEFFFF),
+                modifier = Modifier
+                    .background(Color.Blue)
+            )
+        }
+    }
+}
+
+class Forecast24HoursItem(
+    val time: Long = System.currentTimeMillis(),
+    val temperature: Float = 20.2f,
+    val weatherIconType: Int = 0,
+    val windSpeed: Float = 13.4f
+)
+
+
+class Forecast24HoursItem3(
+    val minTemperature: Float = 20.2f,
+    val maxTemperature: Float = 20.2f,
+)
